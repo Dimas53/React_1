999026b5cdda3c3f041bfecd0d918aeccf5454b4
Dmitriis53 <dima.savcenco@gmail.com>
2019-12-13 21:12:18 +0200

    commit 1

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and not save to reload.
+        </p>
+        <a
+            className="App-link"
+             href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React!!!
+          lorem*10
+          lorem*10
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
